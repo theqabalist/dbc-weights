a261adb207745b7776d25a42404081ae38d8b6ad
Brandon Keown <brandon.keown@taillight.com>
2017-07-06 10:20:57 -0500

    Change directory layout; magic html.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
-// const UglifyEsPlugin = require('uglify-es-webpack-plugin');
+/*eslint camelcase: off*/
+'use strict';
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {filename: 'dist/app.js'},
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.[hash].js'
+    },
     module: {
         loaders: [
-            {test: /\.js$/, loader: 'babel-loader'}
+            {test: /\.js$/, loader: 'babel-loader', query: {compact: false}}
         ]
     },
     plugins: [new BrowserSyncPlugin({
         host: 'localhost',
         port: 3000,
-        server: {baseDir: ['./dist']}
+        server: {baseDir: [path.resolve(__dirname, 'dist')]}
     }), new CopyPlugin([
-        {from: 'index.html', to: 'dist/index.html'},
-        {from: 'styles.css', to: 'dist/styles.css'}
-    ])]
+        {from: './src/css/styles.css'}
+    ]), new HtmlWebpackPlugin({
+        template: './src/ejs/index.ejs'
+    })]
 };
