b8be791fc3077ad466cb10890ce271100989b387
Brandon Keown <keown.brandon@gmail.com>
2017-07-06 16:46:34 -0500

    Repositioned again.

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -9,8 +9,7 @@ const PercTable = require("./perc-table");
 module.exports = class App extends Component {
     render() {
         return div({className: "main ui container"}, [
-            div({className: "stuck-to-bottom"}, [
-                br(),
+            div({className: "stuck-to-top"}, [
                 h(InputStack, this.props.vals)
             ]),
             h(WeightStack, this.props.plates),
--- src/js/components/weight-stack.js
+++ src/js/components/weight-stack.js
@@ -7,7 +7,7 @@ const WomensOutput = require("./womens-output");
 
 module.exports = class WeightStack extends Component {
     render() {
-        return div({className: "ui segments"}, [
+        return div({className: "ui segments", style: {marginTop: "8.5em"}}, [
             div({className: "ui inverted center aligned red segment"}, [h3("Loaded Bar")]),
             div({className: "ui right aligned segment"}, [h(MensOutput, this.props)])
         ]);
